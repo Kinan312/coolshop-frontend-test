adec5f902fdbcacb2c9df40f897509f4aa647f12
kinan311 <kinan.albaroki@gmail.com>
2022-09-17 16:10:30 +0400

    initial commit

--- src/pages/DataTable.js
+++ src/pages/DataTable.js
@@ -1,5 +1,5 @@
 
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useCallback, useState } from "react";
 import EmptyTable from "../components/EmptyTable";
 import RowInput from "../components/Row";
 
